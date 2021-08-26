f232e15c35d2d8053c1c9ca3bdaac38f4e562fde
Lubni Morais <lubni.morais@gmail.com>
2021-08-25 23:36:19 -0300

    chore: Implementing Splash Screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,52 +1,102 @@
-import React, { useCallback } from 'react';
-import { Button, StyleSheet, Dimensions } from 'react-native';
+import React, { useEffect } from 'react';
 
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   Easing,
+  interpolate,
+  Extrapolate,
 } from 'react-native-reanimated';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
 
 import { Container } from './styles';
 
-const WIDTH = Dimensions.get('window').width;
-
 const Splash: React.FC = () => {
-  const animation = useSharedValue(0);
+  const splashAnimation = useSharedValue(0);
 
-  const animatedStyles = useAnimatedStyle(() => {
+  // const brandStyle = useAnimatedStyle(() => {
+  //   return {
+  //     opacity: interpolate(
+  //       splashAnimation.value,
+  //       [0, 25, 50], // coloca as etapas da animação. Tipo de 0 até 25 e de 25 até 50
+  //       [1, 0.3, 0], // qual valor vai ser retornado quando estiver nessa step de animação
+  //       Extrapolate.CLAMP, // para nunca passar do limite
+  //     ),
+  //   };
+  // });
+
+  // const logoStyle = useAnimatedStyle(() => {
+  //   return {
+  //     opacity: interpolate(
+  //       splashAnimation.value,
+  //       [0, 25, 50], // coloca as etapas da animação. Tipo de 0 até 25 e de 25 até 50
+  //       [0, 0.3, 1], // qual valor vai ser retornado quando estiver nessa step de animação
+  //       Extrapolate.CLAMP, // para nunca passar do limite
+  //     ),
+  //   };
+  // });
+
+  const brandStyle = useAnimatedStyle(() => {
     return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 50], // coloca as etapas da animação. Tipo de 0 até 25 e de 25 até 50
+        [1, 0], // qual valor vai ser retornado quando estiver nessa step de animação
+      ),
       transform: [
         {
-          translateX: withTiming(animation.value, {
-            duration: 500,
-            easing: Easing.bounce,
-          }),
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP,
+          ),
         },
       ],
     };
   });
 
-  const handleAnimationPosition = useCallback(() => {
-    animation.value = Math.random() * (WIDTH - 100);
-  }, [animation]);
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50], // coloca as etapas da animação. Tipo de 0 até 25 e de 25 até 50
+        [0, 0.3, 1], // qual valor vai ser retornado quando estiver nessa step de animação
+        Extrapolate.CLAMP, // para nunca passar do limite
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, {
+      duration: 2000,
+    });
+  }, [splashAnimation]);
 
   return (
     <Container>
-      <Animated.View style={[styles.box, animatedStyles]} />
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+        <BrandSvg width={80} height={45} />
+      </Animated.View>
 
-      <Button title="Mover" onPress={handleAnimationPosition} />
+      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
     </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-  },
-});
 
 export { Splash };
